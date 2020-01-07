7174736ee6f1f92a91fffe65f02b92f74b4cd535
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-01-07 19:47:42 +0500

    fix: change context name to rpguia and add first test

--- cypress/integration/Smokes.js
+++ cypress/integration/Smokes.js
@@ -2,6 +2,6 @@
 
 describe('Smokes', function () {
   it('site should have title', function () {
-    cy.visit(Cypress.env('URL')).title().should('eq', "Google")
+    cy.visit(Cypress.env('URL')).title().should('eq', "My react playground!")
   })
 })
