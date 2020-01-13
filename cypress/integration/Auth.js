46e40a44bca60d5e7904c7f2bbd80c71224c5d46
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-01-13 21:38:51 +0500

    fix: fix url in env

--- cypress/integration/Auth.js
+++ cypress/integration/Auth.js
@@ -4,5 +4,5 @@ it('visit URL, type email and password, login', function () {
     cy.visit(Cypress.env('URL'));
     cy.get('[placeholder="Email address"]').type("123");
     cy.get('[placeholder="Password"]').type("123");
-    cy.get('button[class="ui blue large fluid button"]').click({ force: true });
+    cy.get('button[class="ui blue large fluid button"]').click()
 })
