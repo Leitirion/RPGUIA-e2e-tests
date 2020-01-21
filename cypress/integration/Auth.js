dbd8f880d8c860a14899db8f0701d2b297e7d669
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-01-21 20:33:55 +0500

    fix: fix tests for new app

--- cypress/integration/Auth.js
+++ cypress/integration/Auth.js
@@ -1,8 +1,15 @@
 
 
-it('visit URL, type email and password, login', function () {
+it('visit URL, type email and password, sign in, logout', function () {
     cy.visit(Cypress.env('URL'));
-    cy.get('[placeholder="Email address"]').type("123");
-    cy.get('[placeholder="Password"]').type("123");
-    cy.get('button[class="ui blue large fluid button"]').click()
+    cy.get('[id="email"]').click({ force: true }).type('123@gmail.com', { force: true });
+    cy.get('[id="password"]').click({ force: true }).type('123456', { force: true });
+    cy.get('button[id="signin"]').click();
+    cy.wait(1000);
+    cy.get('.title_for_homepage').should('have.text', "Hello to your login page!");
+    cy.get('[id="logout"]').click();
+    cy.wait(1000);
+
+
+    cy.visit(Cypress.env('URL')).title().should('eq', "Welcome to RPGUIA!")
 })
--- cypress/integration/visit.js
+++ cypress/integration/visit.js
@@ -2,6 +2,6 @@
 
 describe('visit', function () {
   it('site should have title', function () {
-    cy.visit(Cypress.env('URL')).title().should('eq', "My react playground!")
+    cy.visit(Cypress.env('URL')).title().should('eq', "Welcome to RPGUIA!")
   })
 })
