b440d96e56be6ec97345d93813305bb26aa95bd2
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-02-18 21:06:14 +0500

    feat: add Todo list test

--- cypress/integration/Todo.js
+++ cypress/integration/Todo.js
@@ -1,12 +1,11 @@
-
-
-it('visit URL, type email and password, sign in, logout', function () {
+it('visit URL, type email and password, sign in, add todo and logout', function () {
     cy.visit(Cypress.env('URL'));
     cy.get('[id="email"]').click({ force: true }).type('123@gmail.com', { force: true });
     cy.get('[id="password"]').click({ force: true }).type('123456', { force: true });
     cy.get('button[id="signin"]').click();
     cy.wait(1000);
-    cy.get('.title_for_homepage').should('have.text', "Hello to your login page!");
+    cy.get('[id="todo"]').click({ force: true }).type('My_test_todo', { force: true }).type('{enter}');
+    cy.get('li[class="todo list-group-item"]').get('span[id="item_text"]').first().should('have.text', "My_test_todo")
     cy.get('[id="logout"]').click();
     cy.wait(1000);
     cy.visit(Cypress.env('URL')).title().should('eq', "Welcome to RPGUIA!")
