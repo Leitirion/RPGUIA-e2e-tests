3957a910a0743d9ff5d71ddb67f4451237729741
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-02-20 18:57:00 +0500

    fix: add click on del button in todo test

--- cypress/integration/Todo.js
+++ cypress/integration/Todo.js
@@ -6,6 +6,8 @@ it('visit URL, type email and password, sign in, add todo and logout', function 
     cy.wait(1000);
     cy.get('[id="todo"]').click({ force: true }).type('My_test_todo', { force: true }).type('{enter}');
     cy.get('li[class="todo list-group-item"]').get('span[id="item_text"]').first().should('have.text', "My_test_todo")
+    cy.get('[id="del_button"]').first().click({ force: true });
+    cy.get('li[class="todo list-group-item"]').get('span[id="item_text"]').first().should('have.text', "A new todo name")
     cy.get('[id="logout"]').click();
     cy.wait(1000);
     cy.visit(Cypress.env('URL')).title().should('eq', "Welcome to RPGUIA!")
