25e3eda57fe0222429fae877a01d8521d270c652
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-03-16 21:40:38 +0500

    fix: fix for todo test

--- cypress/integration/Todo.js
+++ cypress/integration/Todo.js
@@ -4,11 +4,11 @@ it('visit URL, type email and password, sign in, add todo, del todo and logout',
     cy.get('[id="password"]').click({ force: true }).type('123456', { force: true });
     cy.get('button[id="signin"]').click();
     cy.wait(1000);
-    cy.get('[id="todo"]').click({ force: true }).type('My_test_todo', { force: true }).type('{enter}');
-    cy.get('li[class="todo list-group-item"]').get('span[id="item_text"]').first().should('have.text', "My_test_todo")
+    cy.get('[id="todo"]').click({ force: true }).type(`My_test_todo{enter}`);
+    cy.get('li[class="todo list-group-item"]').get('span[id="item_text"]').first().should('have.text', "My_test_todo");
     cy.get('[id="del_button"]').first().click({ force: true });
-    cy.get('li[class="todo list-group-item"]').get('span[id="item_text"]').first().should('have.text', "A new todo name")
+    cy.get('li[class="todo list-group-item"]').get('span[id="item_text"]').first().should('have.text', "A new todo name");
     cy.get('[id="logout"]').click();
     cy.wait(1000);
-    cy.visit(Cypress.env('URL')).title().should('eq', "Welcome to RPGUIA!")
+    cy.visit(Cypress.env('URL')).title().should('eq', "Welcome to RPGUIA!");
 })
